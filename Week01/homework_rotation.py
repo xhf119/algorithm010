2a88f180170c2147428b86aa9fc118b90b6e5bfd
xhf119 <277266319@qq.com>
2020-06-14 22:02:25 +0800

    week01 assigment

---
+++ Week01/homework_rotation.py
@@ -0,0 +1,7 @@
+def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        lenth = len(nums)
+        nums[:] = nums[lenth-k:]+nums[:lenth-k]
+        print(nums)
