eeb11f2cb249b1809236c4d25b154f8cbbd165a1
xhf119 <277266319@qq.com>
2020-07-05 23:26:00 +0800

    homework

--- Week03/NOTE.md
+++ Week03/NOTE.md
@@ -1,1 +1,43 @@
-学习笔记
+﻿学习笔记
+
+树一般都是递归（5-10行搞定）
+1. 不要人肉进行递归（最大误区）
+
+2. 找到最近最简方法，将其拆解成可重复解决的问题（重复子问题）
+if else while loop 递归调用
+3. 数学归纳法思维
+
+Python 代码模板
+# Python
+def recursion(level, param1, param2, ...): 
+    # recursion terminator  递归终结条件
+    if level > MAX_LEVEL: 
+	   process_result 
+	   return 
+    # process logic in current level 处理当前层逻辑
+    process(level, data...) 
+    # drill down                     下探到下一层
+    self.recursion(level + 1, p1, ...) 
+    # reverse the current level status if needed 清理当前层
+
+Java 代码模板
+// Java
+public void recur(int level, int param) { 
+  // terminator 
+  if (level > MAX_LEVEL) { 
+    // process result 
+    return; 
+  }
+  // process current logic 
+  process(level, param); 
+  // drill down 
+  recur( level: level + 1, newParam); 
+  // restore current status 
+ 
+}
+
+
+爬楼梯，括号，二叉搜索树
+
+回溯 分治
+power(x,n) 子集 电话号码字母组合，N皇后
---
+++ Week03/lowestCommonAncestor.py
@@ -0,0 +1,65 @@
+c++ version:
+class Solution {
+public:
+    TreeNode* ans;
+    bool dfs(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr) return false;
+        bool lson = dfs(root->left, p, q);
+        bool rson = dfs(root->right, p, q);
+        if ((lson && rson) || ((root->val == p->val || root->val == q->val) && (lson || rson))) {
+            ans = root;
+        }
+        return lson || rson || (root->val == p->val || root->val == q->val);
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        dfs(root, p, q);
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    TreeNode * lowestCommonAncestor(TreeNode * root, TreeNode * p, TreeNode * q)
+
+
+{
+if (root == NULL)
+return NULL;
+if (root == p | | root == q)
+    return root;
+
+TreeNode * left = lowestCommonAncestor(root->left, p, q);
+TreeNode * right = lowestCommonAncestor(root->right, p, q);
+
+if (left == NULL)
+    return right;
+if (right == NULL)
+    return left;
+if (left & & right) // p和q在两侧
+    return root;
+
+return NULL; // 必须有返回值
+}
+};
+
+python version:
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q: return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left and not right: return # 1.
+        if not left: return right # 3.
+        if not right: return left # 4.
+        return root # 2. if left and right
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q: return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left: return right
+        if not right: return left
+        return root
+
